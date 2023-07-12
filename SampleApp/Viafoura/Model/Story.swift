53c2ea3a9e150d21ca74e48828f03547aead359e
Martin De Simone <martindesimone481@gmail.com>
2023-07-12 14:11:08 -0300

    AuthorUUID

--- SampleApp/Viafoura/Model/Story.swift
+++ SampleApp/Viafoura/Model/Story.swift
@@ -13,6 +13,7 @@ public struct Story {
     let category: String
     let link: String
     let containerId: String
+    let authorId: String
     let storyType: StoryType
 }
 
@@ -22,7 +23,7 @@ enum StoryType {
 }
 
 public let defaultStories: [Story] = [
-    Story(pictureUrl: "https://www.datocms-assets.com/55856/1636753460-information-overload.jpg?crop=focalpoint&fit=crop&fm=webp&fp-x=0.86&fp-y=0.47&h=428&w=856", title: "Moving Staff to Cover the Coronavirus", description: "Here Are What Media Companies Are Doing to Deal With COVID-19 Information Overload", author: "Norman Phillips", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/here-are-what-media-companies-are-doing-with-covid-19-overload", containerId: "101113541", storyType: .comments),
-    Story(pictureUrl: "https://www.datocms-assets.com/55856/1636663477-blognewheights.jpg?fit=crop&fm=webp&h=428&w=856", title: "Grow civility", description: "Don't shut out your community, instead guide them towards civility", author: "Tom Hardington", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/dont-shut-out-your-community-guide-them-to-civility", containerId: "101113531", storyType: .comments),
-    Story(pictureUrl: "https://www.datocms-assets.com/55856/1639925068-brexit-to-cost-the-uk.jpg?fit=crop&fm=webp&h=428&w=856", title: "Brexit cost", description: "Brexit to cost £1,200 for each person in UK", author: "Tom Hardington", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/brexit-to-cost-gbp1-200-for-each-person-in-uk", containerId: "101113509", storyType: .comments)
+    Story(pictureUrl: "https://www.datocms-assets.com/55856/1636753460-information-overload.jpg?crop=focalpoint&fit=crop&fm=webp&fp-x=0.86&fp-y=0.47&h=428&w=856", title: "Moving Staff to Cover the Coronavirus", description: "Here Are What Media Companies Are Doing to Deal With COVID-19 Information Overload", author: "Norman Phillips", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/here-are-what-media-companies-are-doing-with-covid-19-overload", containerId: "101113541", authorId: "", storyType: .comments),
+    Story(pictureUrl: "https://www.datocms-assets.com/55856/1636663477-blognewheights.jpg?fit=crop&fm=webp&h=428&w=856", title: "Grow civility", description: "Don't shut out your community, instead guide them towards civility", author: "Tom Hardington", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/dont-shut-out-your-community-guide-them-to-civility", containerId: "101113531", authorId: "", storyType: .comments),
+    Story(pictureUrl: "https://www.datocms-assets.com/55856/1639925068-brexit-to-cost-the-uk.jpg?fit=crop&fm=webp&h=428&w=856", title: "Brexit cost", description: "Brexit to cost £1,200 for each person in UK", author: "Tom Hardington", category: "ECONOMIA", link: "https://viafoura-mobile-demo.vercel.app/posts/brexit-to-cost-gbp1-200-for-each-person-in-uk", containerId: "101113509", authorId: "", storyType: .comments)
 ]
