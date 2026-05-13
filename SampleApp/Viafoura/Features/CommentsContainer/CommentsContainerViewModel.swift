1d02de8d6f027f38e548a624502e21db9bcd47ae
mdesimone-viafoura <103942744+mdesimone-viafoura@users.noreply.github.com>
2026-05-13 16:40:40 -0300

    LiveQA (#131)
    * LiveQA initial commit
    
    * Remove liveQA
    
    * Add env switcher
    
    * Guard for valid UUID before initializating SDK
    
    * Updates
    
    * Update LiveChatsViewController.swift
    
    * Remove login radius
    
    * Remove packages
    
    * Updates
    
    * Update project.pbxproj
    
    * Update Info.plist
    
    * Update HomeViewController.swift
    
    ---------
    
    Co-authored-by: Martin De Simone <martindesimone481@gmail.com>

--- SampleApp/Viafoura/Features/CommentsContainer/CommentsContainerViewModel.swift
+++ SampleApp/Viafoura/Features/CommentsContainer/CommentsContainerViewModel.swift
@@ -15,6 +15,15 @@ class CommentsContainerViewModel {
     init(story: Story){
         self.story = story
         
-        articleMetadata = VFArticleMetadata(url: URL(string: story.link)!, title: story.title, subtitle: story.description, thumbnailUrl: URL(string: story.pictureUrl)!)
+        let storedDomain = UserDefaults.standard.string(forKey: SettingsKeys.siteDomain)?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let siteDomain = (storedDomain?.isEmpty == false ? storedDomain : nil) ?? SiteDefaults.siteDomain
+
+        let originalUrl = URL(string: story.link)
+        var url = originalUrl
+        if let originalUrl, var components = URLComponents(url: originalUrl, resolvingAgainstBaseURL: false) {
+            components.host = siteDomain
+            url = components.url ?? originalUrl
+        }
+        articleMetadata = VFArticleMetadata(url: url ?? URL(string: "https://\(siteDomain)")!, title: story.title, subtitle: story.description, thumbnailUrl: URL(string: story.pictureUrl)!)
     }
 }
---
+++ SampleApp/Viafoura/Model/SiteDefaults.swift
@@ -0,0 +1,14 @@
+//
+//  SiteDefaults.swift
+//  Viafoura
+//
+//  Created by Codex.
+//
+
+import Foundation
+
+enum SiteDefaults {
+    static let siteUUID = "00000000-0000-4000-8000-c8cddfd7b365"
+    static let siteDomain = "viafoura-mobile-demo.vercel.app"
+}
+
